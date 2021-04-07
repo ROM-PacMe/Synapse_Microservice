fc1356c810d942f8a16ac366e52b79dc3baf6e04
Rafael Pacheco Méndez <rafy.pachecomendez@gmail.com>
2021-04-06 20:43:33 -0400

    add get and post methods

--- src/main/java/org/pacme/synapse/controller/PacmeController.java
+++ src/main/java/org/pacme/synapse/controller/PacmeController.java
@@ -1,5 +1,8 @@
 package org.pacme.synapse.controller;
+import org.pacme.synapse.dto.PersonaDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //Buscar que es una Aplicación Rest (Servicio)
@@ -7,5 +10,11 @@ public class PacmeController {
     @GetMapping("/")
     public String logicMang() {
         return "Este es un pequeño Servicio de respuesta";
+    }
+
+    //DTO
+    @PostMapping("/nombre")
+    public String nombre(@RequestBody PersonaDto personaDto) {
+        return "Tu Nombre es: "+personaDto.getNombre();
     }
 }
---
+++ src/main/java/org/pacme/synapse/dto/PersonaDto.java
@@ -0,0 +1,13 @@
+package org.pacme.synapse.dto;
+
+public class PersonaDto {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
